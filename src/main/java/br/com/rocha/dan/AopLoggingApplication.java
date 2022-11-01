48975ad0d2db30702456e7989bfad186375bb596
Daniel Rocha <daniel_rocha2009@hotmail.com>
2022-11-01 17:36:39 -0300

    Add files via upload

---
+++ src/main/java/br/com/rocha/dan/AopLoggingApplication.java
@@ -0,0 +1,13 @@
+package br.com.rocha.dan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopLoggingApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/rocha/dan/controller/UserController.java
@@ -0,0 +1,28 @@
+package br.com.rocha.dan.controller;
+
+import br.com.rocha.dan.annotation.LogExecutionTime;
+import br.com.rocha.dan.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers(@RequestParam(value = "groupId", required = false) String groupId) {
+        List<User> usersResponse = List.of(new User("João"));
+        return ResponseEntity.ok(usersResponse);
+    }
+
+    @LogExecutionTime
+    @GetMapping("/user")
+    public ResponseEntity<User> getUser() {
+        User user = new User("João");
+        return ResponseEntity.ok(user);
+    }
+
+}
